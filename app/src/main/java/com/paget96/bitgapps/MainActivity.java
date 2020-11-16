2c44ddb2bc5b413d982b6de6ac2a7bd46df39f43
Paget96 <mPaget96@gmail.com>
2020-11-16 17:27:54 +0100

    Added about section Improved app UI

--- app/src/main/java/com/paget96/bitgapps/MainActivity.java
+++ app/src/main/java/com/paget96/bitgapps/MainActivity.java
@@ -2,27 +2,41 @@ package com.paget96.bitgapps;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
+import com.paget96.bitgapps.BuildConfig;
 
 public class MainActivity extends AppCompatActivity {
 
     // Variables
     private final Utils utils = new Utils();
-    private TextView gappsInstallState, gappsPackage, platform, sdk, version, buildDate, buildId, developer;
+    private TextView gappsInstallState, gappsPackage, platform, sdk, version, buildDate, buildId, developer, appVersion;
     private MaterialCardView xda, telegram, gitHub;
     private ImageView expandArrow;
+    private ImageButton about;
     private MaterialButton getBitGapps, exitApp;
-    private LinearLayout buttonHolder, mainLayoutHolder, moreInfo;
+    private LinearLayout buttonHolder, moreInfo, theHitman, paget96;
+    private FrameLayout aboutLayout;
+    private MaterialCardView infoCard;
 
     private void initializeViews() {
-        mainLayoutHolder = findViewById(R.id.main_layout_holder);
+        appVersion = findViewById(R.id.app_version);
+        appVersion.setText(String.format("v%s", BuildConfig.VERSION_NAME));
+
+        aboutLayout = findViewById(R.id.about_gapps);
+        about = findViewById(R.id.about);
+        theHitman = findViewById(R.id.the_hitman);
+        paget96 = findViewById(R.id.paget96);
+        infoCard = findViewById(R.id.info_card);
         gappsInstallState = findViewById(R.id.gapps_install_state);
         buttonHolder = findViewById(R.id.button_holder);
         expandArrow = findViewById(R.id.expand_arrow);
@@ -45,8 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void getText() {
         if (utils.fileExists("/system/etc/g.prop", true)) {
+            infoCard.setCardBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.installed_bg_color));
             gappsInstallState.setText("BitGapps installed");
-            mainLayoutHolder.setClickable(true);
+            infoCard.setClickable(true);
             buttonHolder.setVisibility(View.GONE);
             expandArrow.setVisibility(View.VISIBLE);
 
@@ -59,8 +74,9 @@ public class MainActivity extends AppCompatActivity {
             buildId.setText(String.format("Build ID: %s", utils.splitString(getLineContent(6), "=", 1)));
             developer.setText(String.format("Developer: %s", utils.splitString(getLineContent(7), "=", 1)));
         } else {
+            infoCard.setCardBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.not_installed_bg_color));
             gappsInstallState.setText("BitGapps not installed");
-            mainLayoutHolder.setClickable(false);
+            infoCard.setClickable(false);
             buttonHolder.setVisibility(View.VISIBLE);
             expandArrow.setVisibility(View.GONE);
             moreInfo.setVisibility(View.GONE);
@@ -68,7 +84,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClick() {
-        mainLayoutHolder.setOnClickListener(v -> utils.expandCollapseView(moreInfo, expandArrow));
+        about.setOnClickListener(v -> aboutLayout.setVisibility(View.VISIBLE));
+        aboutLayout.setOnClickListener(v -> aboutLayout.setVisibility(View.GONE));
+        paget96.setOnClickListener(v -> utils.openLink(MainActivity.this, "https://play.google.com/store/apps/dev?id=6924549437581780390&hl=en"));
+        theHitman.setOnClickListener(v -> utils.openLink(MainActivity.this, "https://github.com/TheHitMan7"));
+
+        infoCard.setOnClickListener(v -> utils.expandCollapseView(moreInfo, expandArrow));
         getBitGapps.setOnClickListener(v -> utils.openLink(MainActivity.this, "https://bitgapps.github.io"));
         exitApp.setOnClickListener(v -> finish());
         xda.setOnClickListener(v -> utils.openLink(MainActivity.this, "https://forum.xda-developers.com/android/software/custom-bitgapps-android-t4012165"));
@@ -82,8 +103,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeViews();
-        getText();
         onClick();
+        getText();
     }
 
     public String getLineContent(int line) {
